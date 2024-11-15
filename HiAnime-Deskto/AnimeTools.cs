78aadf6ff3f768cd76dfd87663d208dbdd2d36aa
moaadhzair <92125533+moaadhzair@users.noreply.github.com>
2024-11-15 20:03:26 +0100

    madesome changes

--- HiAnime-Deskto/AnimeTools.cs
+++ HiAnime-Deskto/AnimeTools.cs
@@ -68,122 +68,10 @@ namespace HiAnime_Desktop
             return null;
 
         }
-        /*public static async Task<int> processEpsiodes(AnimeInfo anime)
-        {
-            //this function processes each episode and retrieves it subbed and dubbed servers
-            var id = anime.animeLink.Substring(anime.animeLink.LastIndexOf("-") + 1);
-            var Episodes = await new HttpClient().GetStringAsync("https://hianime.to/ajax/v2/episode/list/" + id);
-
-
-            var jsonDoc = JsonDocument.Parse(Episodes);
-            string htmlContent = jsonDoc.RootElement.GetProperty("html").GetString();
-
-            // Load HTML for parsing
-            var htmlDoc = new HtmlAgilityPack.HtmlDocument();
-            htmlDoc.LoadHtml(htmlContent);
-
-            // Extract episode names and links
-            var episodes = new List<Episode>();
-            var list = htmlDoc.DocumentNode.FirstChild.ChildNodes[1].ChildNodes[1].ChildNodes[3];
-
-            cleanList(list);
-
-            //Console.WriteLine("wdw");
-
-            foreach (var node in list.ChildNodes)
-            {
-                string linkk = "https://hianime.to" + node.Attributes["href"].Value;
-                string name = node.Attributes["title"].Value;
-                int ID = int.Parse(node.Attributes["data-id"].Value);
-
-
-                jsonDoc = JsonDocument.Parse(await new HttpClient().GetStringAsync("https://hianime.to/ajax/v2/episode/servers?episodeId=" + ID));
-                string html = jsonDoc.RootElement.GetProperty("html").ToString();
-
-                var htmlDoc2 = new HtmlAgilityPack.HtmlDocument();
-
-                htmlDoc2.LoadHtml(html);
-                var SubbedList = htmlDoc2.DocumentNode.ChildNodes[2].ChildNodes[3];
-                cleanList(SubbedList);
-                var SubbedServers = new List<(String ServerName, String ServerEmbedLink)>();
-                foreach (var item in SubbedList.ChildNodes)
-                {
-                    jsonDoc = JsonDocument.Parse(await new HttpClient().GetStringAsync("https://hianime.to/ajax/v2/episode/sources?id=" + item.Attributes["data-id"].Value));
-                    SubbedServers.Add((item.ChildNodes[1].InnerHtml, jsonDoc.RootElement.GetProperty("link").ToString()));
-                }
-
-
-                var DubbedList = htmlDoc2.DocumentNode.ChildNodes[4].ChildNodes[3];
-                cleanList(DubbedList);
-                var DubbedServers = new List<(String ServerName, String ServerEmbedLink)>();
-                foreach (var item in DubbedList.ChildNodes)
-                {
-                    jsonDoc = JsonDocument.Parse(await new HttpClient().GetStringAsync("https://hianime.to/ajax/v2/episode/sources?id=" + item.Attributes["data-id"].Value));
-                    DubbedServers.Add((item.ChildNodes[1].InnerHtml, jsonDoc.RootElement.GetProperty("link").ToString()));
-                }
-
-                episodes.Add(new Episode() { isDubbed = DubbedServers.Count > 0, Name = name, SubbedServers = SubbedServers, DubbedServers = DubbedServers, Link = linkk, ID = ID });
-
-            }
-            anime.Episodes = episodes;
-            return 0;
-        }
-
-        */
+        
         public static async Task<int> processEpsiodeServers(/*AnimeInfo anime, int ep*/ Episode episode)
         {
-            //this function processes a certain episode @{ep} and retrieves it subbed and dubbed servers
-            /*var id = anime.animeLink.Substring(anime.animeLink.LastIndexOf("-") + 1);
-            var Episodes = await new HttpClient().GetStringAsync("https://hianime.to/ajax/v2/episode/list/" + id);
-
-
-            var jsonDoc = JsonDocument.Parse(Episodes);
-            string htmlContent = jsonDoc.RootElement.GetProperty("html").GetString();
-
-            // Load HTML for parsing
-            var htmlDoc = new HtmlAgilityPack.HtmlDocument();
-            htmlDoc.LoadHtml(htmlContent);
-
-            // Extract episode names and links
-            var episodes = new List<Episode>();
-
-            int EpisodeCount = jsonDoc.RootElement.GetProperty("totalItems").GetInt32();
-
-            for (int i = 0; i < EpisodeCount; i++)
-            {
-                episodes.Add(null);
-            }
-
-
-
-            if (EpisodeCount < ep) throw new Exception($"{anime.name} doesn't have the {ep} Episode");
-            int pages = (EpisodeCount + 100 - 1) / 100;
-            HtmlNode list = null;
-            HtmlNode Animenode = null;
-            if (pages > 1)
-            {
-                var Pagelist = htmlDoc.DocumentNode.FirstChild.ChildNodes[1].ChildNodes[1];
-                cleanList(Pagelist);
-                Pagelist.RemoveChild(Pagelist.FirstChild);
-                var dontknow = Pagelist.ChildNodes[-1 + (ep + 100 - 1) / 100];
-                cleanList(dontknow);
-                int currentEp = 10 + ep - (1000 + (-1 + (ep + 100 - 1) / 100));
-                Animenode = dontknow.ChildNodes[currentEp];
-
-            }
-            else
-            {
-                Animenode = list.FirstChild.ChildNodes[ep];
-            }
-
-
-
-
-            string linkk = "https://hianime.to" + Animenode.Attributes["href"].Value;
-            string name = Animenode.Attributes["title"].Value;
-            int ID = int.Parse(Animenode.Attributes["data-id"].Value);
-            */
-
+            
             var episodes = new List<Episode>();
 
 
@@ -232,19 +120,10 @@ namespace HiAnime_Desktop
 
             }
 
-
-
-
-
-            //episodes.Insert(ep, new Episode() { Number = ep, isDubbed = DubbedList != null, Name = name, SubbedServers = SubbedServers, DubbedServers = DubbedServers, Link = linkk, ID = ID });
-
             episode.isDubbed = DubbedList != null;
             episode.SubbedServers = SubbedServers;
             episode.DubbedServers = DubbedServers;
 
-
-
-            //anime.Episodes = episodes;
             return 0;
         }
 
@@ -273,12 +152,8 @@ namespace HiAnime_Desktop
                 episodes.Add(null);
             }
 
-
-
-            //if (EpisodeCount < ep) throw new Exception($"{anime.name} doesn't have the {ep} Episode");
             int pages = (EpisodeCount + 100 - 1) / 100;
             HtmlNode list = null;
-            //HtmlNode Animenode = null;
 
             var Pagelist = htmlDoc.DocumentNode.FirstChild.ChildNodes[1].ChildNodes[1];
             cleanList(Pagelist);
@@ -300,20 +175,7 @@ namespace HiAnime_Desktop
                 }
                 
             }
-            
-                
-                
-                /*var currentPage = Pagelist.ChildNodes[-1 + (ep + 100 - 1) / 100];
-                cleanList(currentPage);
-                int currentEp = 10 + ep - (1000 + (-1 + (ep + 100 - 1) / 100));
-                Animenode = currentPage.ChildNodes[currentEp];
-
-            
-            */
-
-
-
-            
+                 
             anime.Episodes = episodes;
             return 0;
         }
