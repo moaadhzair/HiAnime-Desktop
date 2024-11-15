fc61d692ec322886592eb18d0b2a66c945d03469
moaadhzair <92125533+moaadhzair@users.noreply.github.com>
2024-11-15 20:27:05 +0100

    doknow

---
+++ HiAnime Desktop/AnimeInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace HiAnime_Desktop
+{
+    public class AnimeInfo
+    {
+        public string name { get; set; }
+        public string posterLink { get; set; }
+        public int subbedEpisodes { get; set; }
+        public int dubbedEpisodes { get; set; }
+        public string animeLink { get; set; }
+        public List<Episode> Episodes { get; set; }
+        public int EpsiodeCount { get; set; }
+
+        /*public AnimeInfo(string name, string posterLink, int subbedEpisodes, int dubbedEpisodes, string animeLink, int epsiodeCount)
+        {
+            this.name = name;
+            this.posterLink = posterLink;
+            this.subbedEpisodes = subbedEpisodes;
+            this.dubbedEpisodes = dubbedEpisodes;
+            this.animeLink = animeLink;
+            EpsiodeCount = epsiodeCount;
+        }*/
+    }
+}
